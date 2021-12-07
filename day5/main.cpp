0ade5d5ffe3fb9ad05b3d9b09c960bde241206c4
Irwin08 <jesse.irwin11@gmail.com>
2021-12-07 07:24:52 -0800

    day 5 setup

---
+++ day5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+
+
+std::array<std::array<int, 1000>, 1000> generate_map() {
+    std::array<std::array<int, 1000>, 1000> map {};
+}
+
+
+int main() {
+
+    return 0;
+}
