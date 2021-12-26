7697ce3ff96e9cf8b2ba54cd8a45a4d4150430b4
Irwin08 <jesse.irwin11@gmail.com>
2021-12-25 16:22:10 -0800

    Day 9 complete!

--- day9/main.cpp
+++ day9/main.cpp
@@ -32,6 +32,68 @@ std::vector<int> low_points_risk(Graph<int>& graph) {
     return low_points;
 }
 
+std::vector<std::weak_ptr<Node<int>>> low_points_vec(Graph<int>& graph) {
+
+    std::vector<std::weak_ptr<Node<int>>> low_points {};
+
+    for(int i = 0; i < graph.size(); i++) {
+        auto node_ptr = graph.get(i);
+        if(auto node = node_ptr.lock()) {
+            if(std::all_of(node->edges.begin(), node->edges.end(), [&](auto& nbhr){
+                if(auto nbhr_ptr = nbhr.lock()) {
+                    return (nbhr_ptr->data > node->data);
+                }
+                else {
+                    std::cerr << "Unable to lock node" << std::endl;
+                    return false;
+                }
+             })) {
+                 low_points.push_back(node_ptr);
+             }
+        }
+        else {
+            std::cerr << "Unable to lock node" << std::endl;
+        }
+    }
+    return low_points;
+}
+
+void calculate_basin(std::weak_ptr<Node<int>> start, std::vector<std::weak_ptr<Node<int>>>& visited) {
+
+    visited.push_back(start);
+
+    if(auto n = start.lock()) {
+
+        for(auto e : n->edges) {
+            if(std::find_if(visited.begin(), visited.end(), [&](const auto& wk_ptr){return wk_ptr.lock() == e.lock();}) == visited.end()) {
+                //haven't yet traversed
+                if(auto next = e.lock()) {
+                    if((next->data >= n->data) && (next->data != 9)) {
+                        //flows into current
+                        calculate_basin(e, visited);
+                    }
+                }
+            }
+        } 
+    }
+}
+
+void basins(std::vector<std::weak_ptr<Node<int>>> low_points) {
+    std::vector<int> basin_sizes {};
+    for(auto low : low_points) {
+        std::vector<std::weak_ptr<Node<int>>> visited {};
+        calculate_basin(low, visited);
+        basin_sizes.push_back(visited.size());
+    }
+
+    std::cout << "Number of basins: " << basin_sizes.size() << std::endl;
+
+    std::sort(basin_sizes.begin(), basin_sizes.end(), [](int i, int j){ return i > j; });
+
+    // ASSUME 3 BASINS EXIST
+    std::cout << "Product of top 3 basins: " << (basin_sizes[0] * basin_sizes[1] * basin_sizes[2]) << std::endl;
+}
+
 
 int main() {
 
@@ -88,6 +150,11 @@ int main() {
     auto sum = std::accumulate(low.begin(), low.end(), low.size());
 
     std::cout <<  "Sum of risk: " << sum << std::endl;
+
+
+    //need to calculate basins. Will model as vector of vectors of nodes
+
+    basins(low_points_vec(graph));
 
 
     return 0;
