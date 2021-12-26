bde3349df2a9c462aafffbbe22c3a352c7925c8c
Irwin08 <jesse.irwin11@gmail.com>
2021-12-25 19:09:05 -0800

    day 10 complete

--- day10/main.cpp
+++ day10/main.cpp
@@ -2,6 +2,9 @@
 #include <stack>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 struct Line {
     std::stack<char> opening {};
@@ -9,11 +12,12 @@ struct Line {
 
     std::vector<char> opening_chars = {'[', '{', '<', '('};
     std::vector<char> closing_chars = {']', '}', '>', ')'};
+    
 
     //returns line with error, -1 if good
     int process(std::string line) {
         for(int i = 0; i < line.length(); i++) {
-            if((closing.empty()) && (std::find(closing_chars.begin(), closing_chars.end(), line[i])!= closing_chars.end())) {
+            if((opening.empty()) && (std::find(closing_chars.begin(), closing_chars.end(), line[i])!= closing_chars.end())) {
                 return i;
             }
             auto it_closing = std::find(closing_chars.begin(), closing_chars.end(), line[i]);
@@ -38,6 +42,27 @@ struct Line {
 
         return -1;
     }
+
+    std::string complete(std::string line) {
+        opening = std::stack<char>{};
+        closing = std::stack<char>{};
+
+        process(line);
+
+        std::string complete {};
+
+        while(opening.size() > closing.size()) {
+            auto opener = opening.top();
+            auto it = std::find(opening_chars.begin(), opening_chars.end(), opener);
+            int index = std::distance(opening_chars.begin(), it);
+
+            complete.push_back(closing_chars[index]);
+
+            opening.pop();
+        }
+
+        return complete;
+    }
 };
 
 
@@ -45,7 +70,42 @@ int main() {
 
     Line line {};
 
-    std::cout << line.process(std::string("([])")) << std::endl;
+    std::ifstream file {"input.txt"};
+    std::string s;
+
+    std::vector<int> points = {57, 1197, 25137, 3};
+    std::vector<char> closing_chars = {']', '}', '>', ')'};
+
+    int score = 0;
+    std::vector<unsigned long long> completion_scores {};
+    while(std::getline(file, s)) {
+        int round = line.process(s);
+        if(round != -1) {
+            auto it = std::find(closing_chars.begin(), closing_chars.end(), s[round]);
+            score += points[it - closing_chars.begin()];
+    
+        }
+        else {
+            std::vector<int> score_guide {2, 3, 4, 1};
+            auto str = line.complete(s);
+            unsigned long long score_2 = 0;
+
+            for(int i = 0; i < str.length(); i++) {
+                auto it = std::find(closing_chars.begin(), closing_chars.end(), str[i]);
+                int index = std::distance(closing_chars.begin(), it);
+                score_2 = (score_2*5) + (unsigned long long)score_guide[index];
+            }
+
+            completion_scores.push_back(score_2);
+            
+        }
+    }
+
+    std::cout << "Score: " << score << std::endl;
+
+    std::sort(completion_scores.begin(), completion_scores.end(), [](unsigned long long i, unsigned long long j){return i > j;});
+
+    std::cout << "Middle score: " << completion_scores[(completion_scores.size()-1)/2] << std::endl;
 
     return 0;
 }
